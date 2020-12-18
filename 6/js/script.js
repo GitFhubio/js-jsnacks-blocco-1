58260ab8d918bc2547411cddebc7a9d5c5367d40
Fabio Petrone <fa610movic90@gmail.com>
2020-12-18 20:29:57 +0100

    precisazioni

--- 6/js/script.js
+++ 6/js/script.js
@@ -5,7 +5,7 @@ var N = parseInt(prompt("Di quanti numeri vuoi ottenere il cubo? "));
 if(N>10){
   alert('Non puoi proseguire');}
   else{
-  for (var i = 0; i < N; i++) {
+  for (var i = 1; i <= N; i++) {
       console.log(Math.pow(i, 3));
   }
 }
