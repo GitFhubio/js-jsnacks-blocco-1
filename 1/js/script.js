dfa63fdf81163f279e6f226d2dcfe8a7339b1601
Fabio Petrone <fa610movic90@gmail.com>
2020-12-18 18:06:08 +0100

    .

--- 1/js/script.js
+++ 1/js/script.js
@@ -1,11 +1,11 @@
 /* 1) L'utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore */
 
-var number1 = parseInt(prompt("Insert a number"));
-var number2 = parseInt(prompt("Insert another number"));
+var number1 = parseInt(prompt("Inserisci un numero"));
+var number2 = parseInt(prompt("Inserisci un altro numero"));
 
 if (number1 > number2) {
-  console.log("The highest number is: ", number1);
+  console.log("Il numero maggiore è: ", number1);
 } else if(number1 < number2){
-  console.log("The highest number is: ", number2);
+  console.log("Il numero maggiore è: ", number2);
 }
 else{ console.log('I due numeri sono uguali');}
