850d09d7c085a13ef1048a205c509c6f520fbe7b
Fabio Petrone <fa610movic90@gmail.com>
2020-12-18 16:10:11 +0100

    alternative

--- 7/js/script.js
+++ 7/js/script.js
@@ -1,5 +1,11 @@
-// 7: Stampa le potenze di 2 fino a 1000. 
+// 7: Stampa le potenze di 2 fino a 1000.
 
 for (var i = 2; i <= 1000; i++) {
     console.log(Math.pow(2,i));
 }
+
+// Stampa le potenze di 2 fino a 1000.
+
+// for (var i = 2; i <= Math.pow(2,1000); i=i*2) {
+//     console.log(i);
+// }
